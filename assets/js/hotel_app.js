5274f16f7d943c433db0875de61f85b6ac41796a
Medric Riley <medricr@gmail.com>
2019-04-29 17:11:18 -0700

    added placeholer background image, and fleshed out hotel display list

--- assets/js/hotel_app.js
+++ assets/js/hotel_app.js
@@ -36,9 +36,12 @@ $(document).ready(()=>{
         var dates_of_visit = [];
         dates_of_visit.push($("#arrival_date_entry").val().trim());
         dates_of_visit.push($("#departure_date_entry").val().trim());
+        if(dates_of_visit[1] == null){
+            dates_of_visit[1] = dates_of_visit[0];
+        }
 
-        console.log(location);
-        console.log(dates_of_visit);
+        // console.log(location);
+        // console.log(dates_of_visit);
 
         // Nested API calls which allow the user to see hotels in the destination city
     $.ajax({
@@ -49,15 +52,33 @@ $(document).ready(()=>{
         url: "https://apidojo-kayak-v1.p.rapidapi.com/locations/search?where=" + location,
         method: "GET"
     }).then(function(response){
+        // console.log(response);
+        // retrieve city code 
+        var city_code = response[1].ctid;
+        // console.log(city_code);
         $.ajax({
             beforeSend: function(request){
                 request.setRequestHeader("X-RapidAPI-Host","apidojo-kayak-v1.p.rapidapi.com");
                 request.setRequestHeader("X-RapidAPI-Key","da97cf968cmsha16cf672fa90a65p1735a7jsnc510610589d5");
             },
-            url: "https://apidojo-kayak-v1.p.rapidapi.com/hotels/create-session?&rooms=1&citycode=16078&checkin=2019-07-20&checkout=2019-07-24&adults=1",
+            url: "https://apidojo-kayak-v1.p.rapidapi.com/hotels/create-session?&rooms=1&citycode=" + city_code + "&checkin="+ dates_of_visit[0] + "&checkout="+ dates_of_visit[1] + "&adults=1",
             method: "GET"
         }).then(function(response){
-            console.log(response);
+            // console.log(response);
+            console.log(response.hotelset)
+            if(response.hotelset.length == 0){
+                alert("no results found!");
+            }
+            for(let i=0; i<response.hotelset.length; i++){
+                $(".hotel_display").append(
+                    "<tr><td>" + i + "</td>" + "<td>" + response.hotelset[i].name + "</td>"
+                    + "<td>" + response.hotelset[i].price + "</td>" + "<td>" + response.hotelset[i].address 
+                    + "</td>" + "<td>" + "<a href=" + response.baseUrl + response.hotelset[i].shareURL + " target='blank'>Get a room</a>"
+                    + "</td></tr>"
+                );
+            }
+            
+
         })
     })
     })
