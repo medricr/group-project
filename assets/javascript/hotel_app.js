291d62addb742a6d3945686169f141f35edfb128
Medric Riley <medricr@gmail.com>
2019-05-03 11:07:04 -0700

    added features such that the hotel markers pop up on the map when a hotel row is clicked

--- assets/javascript/hotel_app.js
+++ assets/javascript/hotel_app.js
@@ -1,34 +1,12 @@
-/* 
-HOTEL_APP.JS PSEUDOCODE
-
-    begin
-        prompt user for input on location / get location/date data from another part of app
-            with that information...
-                store the date data in a variable called date(s)_of_visit
-                make a call to the location API to return the citycode of the city that the event is in
-            using that citycode...
-                make a call to the hotel API to return a list of hotels in the area
-        display the hotel information in the appropriate div
-
-HOTEL_APP.JS VARIABLE PROTOTYPES
-
-    location (string) - A variable holding either the name of the airport in the destination city, or the airport code of the destination city 
-
-    dates_of_visit (array of strings) - an array which will hold the date of arrival in the first position, and the date of departure in the second position. The date of departure is optional, and will be set to null if the user does not input one
-
-*/
+// dates of visit (array) - required for calling the Kajak hotel APO
 var dates_of_visit = [];
 var hotel_index = 0;
+// hotel coordinates (array) - holds the latitude and longitude of the retrieved hotels
 var hotle_coords = [];
 $(document).ready(()=>{
-    // $("#submit_btn").on("click",()=>{
-    //     var location = $("#city_entry").val().trim();
     $("#submit").on("click",()=>{
-        // alert($("#city").val().trim());
         var location = $("#city").val().trim();
         var dates_of_visit=[];
-        // dates_of_visit[0] = ($("#arrival_date_entry").val().trim());
-        // dates_of_visit[1] = ($("#departure_date_entry").val().trim());
         // if the user hasn't entered an initial date...
         if(dates_of_visit[0] == null){
             // get the current date...
@@ -61,32 +39,53 @@ $(document).ready(()=>{
                 url: "https://apidojo-kayak-v1.p.rapidapi.com/hotels/create-session?&rooms=1&citycode=" + city_code + "&checkin="+ dates_of_visit[0] + "&checkout="+ dates_of_visit[1] + "&adults=1",
                 method: "GET"
             }).then(function(response){
-                // console.log(response);
-                console.log(response.hotelset)
-                
                 if(response.hotelset.length == 0){
                     alert("no results found!");
                 }
                 for(let i=0; i<10; i++){
+                    console.log("init add fire");
                     $(".hotel_display").append(
-                        // add hotel class and unique identifier attributes
-                        '<tr class="hotel hotel_'+i+'" data_lat="'+response.hotelset[i].lat+'" data_lon="'+response.hotelset[i].lon+'" >'
-                        // (i+1) 
-                        // add hotel name
-                        // + "</td>" + "<td>" + 
+                        // open table & give each table entry its appropriate lat/lon data attribute, name attribute, and link attribute...
+                        '<tr class="hotel" data_lat="'+response.hotelset[i].lat+'" data_lon="'+response.hotelset[i].lon+'" data_name="' + response.hotelset[i].name + '" data_link="'+response.baseUrl + response.hotelset[i].shareURL +'" >'
+                        // populate table name field...
                         + "<td>" + response.hotelset[i].name + "</td>"
+                        // populate table avg price field...
                         + "<td>" + "$" + ((parseFloat(response.hotelset[i].pricehistoryhi,10) + parseFloat(response.hotelset[i].pricehistorylo,10))/2) + "</td>" 
+                        // populate table address field...
                         + "<td>" + response.hotelset[i].address + "</td>" 
-                        + "<td>" +  response.hotelset[i].neighborhood+ "</td>" 
-                        + "<td>" + "<a href=" + response.baseUrl + response.hotelset[i].shareURL + " target='blank'>Book Now!</a>" + "</td>" 
-                        + "<td>" + '<button class="btn btn-primary">Add To Map</button>' + "</td></tr>"
+                        // populate table neighborhood field...
+                        + "<td>" +  response.hotelset[i].neighborhood
+                        // close table...
+                        + "</td></tr>"
                     );
+                    // and update the current position in the hotelset array
                     hotel_index++;
+                    console.log(hotel_index);
                 } 
-            })
-        })
-    })
-    
-    var d = new Date();
-    console.log(d.toISOString().slice(0,10));
-})
+                // when the user clicks the "show more" button...
+                $(document.body).on("click",".more_btn",function(){
+                    var index = hotel_index;
+                    // get the current position in the hotelset array....
+                    for(let i=index; i<(index+10); i++){
+                        $(".hotel_display").append(
+                            // open table & give each table entry its appropriate lat/lon data attribute, name attribute, and link attribute...
+                            '<tr class="hotel" data_lat="'+response.hotelset[i].lat+'" data_lon="'+response.hotelset[i].lon+'" data_name="' + response.hotelset[i].name + '" data_link="'+response.baseUrl + response.hotelset[i].shareURL +'" >'
+                            // populate table name field...
+                            + "<td>" + response.hotelset[i].name + "</td>"
+                            // populate table avg price field...
+                            + "<td>" + "$" + ((parseFloat(response.hotelset[i].pricehistoryhi,10) + parseFloat(response.hotelset[i].pricehistorylo,10))/2) + "</td>" 
+                            // populate table address field...
+                            + "<td>" + response.hotelset[i].address + "</td>" 
+                            // populate table neighborhood field...
+                            + "<td>" +  response.hotelset[i].neighborhood
+                            // close table...
+                            + "</td></tr>"
+                        );
+                        // and update the current position in the hotelset array
+                        hotel_index++;
+                    }
+                });
+            });
+        });
+    });
+});
--- assets/javascript/maps.js
+++ assets/javascript/maps.js
@@ -23,123 +23,21 @@ $(document).ready(function () {
 
     getLocation();
 
-    // $(document.body).on("click", "#mapBtn", function () {
-        // var eventLong = $(this).attr("data-longitude");
-        // var eventLat = $(this).attr("data-latitude");
-        // var eventCoordinates = [eventLong, eventLat];
-        // console.log(eventCoordinates);
-
-        // var canvas = map.getCanvasContainer();
-
-        // var geojson = {
-        //     "type": "FeatureCollection",
-        //     "features": [{
-        //         "type": "Feature",
-        //         "geometry": {
-        //             "type": "Point",
-        //             "coordinates": eventCoordinates
-        //         }
-        //     }]
-        // };
-
-        // // Add a single point to the map
-        // map.addSource('point', {
-        //     "type": "geojson",
-        //     "data": geojson
-        // });
-
-    //     map.addLayer({
-    //         "id": "point",
-    //         "type": "circle",
-    //         "source": "point",
-    //         "paint": {
-    //             "circle-radius": 10,
-    //             "circle-color": "orange"
-    //         }
-    //     });
-
-    //     // var geocoder = new MapboxGeocoder({
-    //     //     accessToken: mapboxgl.accessToken,
-    //     //     marker: {
-    //     //         color: 'orange',
-    //     //         geography: location
-    //     //     },
-    //     //     mapboxgl: mapboxgl
-    // });
-
-    // $(document.body).on("slide.bs.carousel",function(event){
-    //     var index = event.to;
-    //     var event_lat = latitudes[index];
-    //     var event_lon = longitues[index];
-    //     var event_coords = [event_lon,event_lat];
-
-    //     var geo_json = {
-    //         "type": "FeatureCollection",
-    //         "features": [{
-    //             "type": "Feature",
-    //             "geometry": {
-    //                 "type": "Point",
-    //                 "coordinates": event_coords
-    //             }
-    //         }]
-    //     };
-
-    //     // and add it to the map
-    //     map.addSource('point', {
-    //         "type": "geojson",
-    //         "data": geo_json
-    //     });
-
-    //     map.addLayer({
-    //         "id": "point",
-    //         "type": "circle",
-    //         "source": "point",
-    //         "paint": {
-    //             "circle-radius": 10,
-    //             "circle-color": "orange"
-    //         }
-    //     });  
-
-
-    // });
-
     // when the user clicks on a hotel....
     $(document.body).on("click",".hotel", function(){
         // get that hotels lang/lot coordinates...
         var hotel_lat = $(this).attr("data_lat");
-        var hotel_lon = $(this).attry("data_lon");
+        var hotel_lon = $(this).attr("data_lon");
         // store them in a hotel coords variable
         var hotel_coords = [hotel_lon,hotel_lat];
 
-        var canvas = map.getCanvasContainer();
-
-        // store it in a json variable...
-        var geo_json = {
-            "type": "FeatureCollection",
-            "features": [{
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": hotel_coords
-                }
-            }]
-        };
-
-        // and add it to the map
-        map.addSource('point', {
-            "type": "geojson",
-            "data": geo_json
-        });
-
-        map.addLayer({
-            "id": "point",
-            "type": "circle",
-            "source": "point",
-            "paint": {
-                "circle-radius": 10,
-                "circle-color": "blue"
-            }
-        });
+        var el = document.createElement("div");
+        el.className = "marker";
+        new mapboxgl.Marker(el)
+        .setLngLat(hotel_coords)
+        .setPopup(new mapboxgl.Popup({ offset: 25 })
+            .setHTML('<h3>' + $(this).attr("data_name") + '</h3><p>' + "<a href="+$(this).attr("data_link")+'">Book Now!</a>' + '</p>'))
+        .addTo(map);
     });
     $('#carousel').on('slide.bs.carousel', function (event) {
 
