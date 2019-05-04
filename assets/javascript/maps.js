8fb701a5cd3cbc9cd286c76b1412978489411706
YaroslavPetsenyuk <47685122+YaroslavPetsenyuk@users.noreply.github.com>
2019-05-04 10:21:30 -0700

    Revert "Medric branch"

--- assets/javascript/maps.js
+++ assets/javascript/maps.js
@@ -26,29 +26,6 @@ $(document).ready(function () {
             $(".marker").remove();
         }
 
-    // when the user clicks on a hotel....
-    $(document.body).on("click",".hotel", function(){
-        // remove any currently places hotel markers...
-        if($(".hotel_marker")) {
-            $(".hotel_marker").remove();
-        }
-        // get that hotels lang/lot coordinates...
-        var hotel_lat = $(this).attr("data_lat");
-        var hotel_lon = $(this).attr("data_lon");
-        // store them in a hotel coords variable
-        var hotel_coords = [hotel_lon,hotel_lat];
-
-        var el = document.createElement("div");
-        el.className = "marker hotel_marker";
-        new mapboxgl.Marker(el)
-        .setLngLat(hotel_coords)
-        .setPopup(new mapboxgl.Popup({ offset: 25 })
-            .setHTML('<h3>' + $(this).attr("data_name") + '</h3><p>' + "<a href="+$(this).attr("data_link")+'" target="_blank">Book Now!</a>' + '<button class="hotel_remove_btn btn">Remove</button>' + '</p>'))
-        .addTo(map);
-    });
-
-    $('#carousel').on('slide.bs.carousel', function (event) {
-
         $(".mapboxgl-popup").remove();
 
         var index;
